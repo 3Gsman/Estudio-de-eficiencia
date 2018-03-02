32498c78645b9ba8c7573f6bcac4e59310340c76
PabloFerrer <pablo@certrans.es>
2018-03-02 13:37:06 +0100

    Comentarios JavaDoc de la clase

--- src/Alg.java
+++ src/Alg.java
@@ -1,13 +1,37 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
+/**
+ * La clase Alg.java es aquella en la que tenemos los algoritmos de ordenacion y de busqueda
+ * MergeSort, Quicksort, BinarySearch y SecuentialSearch.
+ * En ella para cada metodo de ordenacion y busqueda creamos un array de 100 posiciones para el cual 
+ * vamos introduciendo cantidades de elementos que varian entre 5000, 7500, 10000 y 12500.
+ * Tras crear los arrays con sus elementos hacemos testeos de los algoritmos en base al tiempo que tardan
+ * y en concreto para los algoritmos de ordenacion, la memoria del sistema que utilizan.
+ * Acto seguido los datos son enviados a ficheros excel.csv. 
+ * 
+ * @author German Garcia Garcia && Pablo Ferrer Lopez.
+ */
 public class Alg {
 
     //ORDENACIONES:
 	
     //MERGE SORT:
 	
+	/**
+	 * El algoritmo MergeSort es un metodo de ordenacion recursivo por mezcla ,que a traves de la tecnica
+	 * divide y venceras, divide el array en unidades lo mas pequeñas posibles (estando desordenadas)
+	 * y a continuacion compara cada una y la coloca en su posicion correspondiente (ordenandolas)
+	 * de forma que se vallan agrupando en unidades mas grandes y con estas vuelve a comparar los elementos
+	 * entre los grupos que tenemos hasta tener nuestro array ordenado.
+	 *  
+	 * @param Como parametros que le pasamos tenemos: Un Array de numeros enteros, la primera posicion y
+	 * 		  la ultima posicion
+	 * 
+	 * @param int[] numbers
+	 * @param low
+	 * @param high
+	 */
     public static void mergeSort(int[] numbers, int low, int high) {
         if (low < high) {
             int middle = low + (high - low) / 2;
@@ -47,6 +71,13 @@ public class Alg {
 
     //QUICK SORT:
     
+    /**
+     * 
+     * @param numbers
+     * @param low
+     * @param high
+     */
+    
     public static void quickSort(int[] numbers, int low, int high) {
         if (low < high) {
             int posicionPivote = pivot(numbers, low, high);
@@ -79,6 +110,22 @@ public class Alg {
     //BUSQUEDAS:
     
     //BUSQUEDA SECUANCIAL:
+    
+    /**
+     * El algoritmo de BusquedaSecuencial es un metodo boolean que nos permite realizar una busqueda de un 
+     * elemento en un Array de elementos de forma que recorre el Array y compara el elemento en cuestion
+     * y si es ese el buscado nos devolvera true y si no lo es continuara avanzando posiciones una a una 
+     * hasta encontrar el elemento dado.
+     * Si no lo encuentra al recorrer el Array hasta el final entonces devolvera false.
+     * 
+     * @param Como parametros que le pasamos tenemos: Un Array de numeros enteros y el elemento a buscar.
+     * 
+     * @param int[] lista
+     * @param int x
+     * 
+     * @return Devolvera true o false dependiendo de si encuentra el elemento o no.
+     */
+    
     public static boolean busquedaSecuencial(int[] lista, int x ){
 		int i = 0;
     	while (lista[i]!=x && i < lista.length -1){
@@ -94,6 +141,28 @@ public class Alg {
     }
 
     //BUSQUEDA BINARIA:
+    
+    /**
+     * El algoritmo de BusquedaBinaria es un metodo recursivo boolean que nos permite 
+     * realizar una busqueda de un elemento en un Array a traves de la tecnica divide y
+     * venceras. Consiste en comparar el valor con el elemento que se encuentra en la
+     * posicion que ocupa el medio del Array y verificar si este es nuestro elemento buscado.
+     * Si lo es entonces directamente devolvera true pero si no lo es entonces comprobamos si 
+     * nuestro valor a buscar es mayor que el elemento que ocupa la posicion del medio y si 
+     * es mayor entonces aplicamos el metodo recursivo pasandole como inicio 
+     * la mitad del Array + 1 y como final simplemente el final del Array. En caso de que sea menor
+     * hacemos la misma operacion pero esta vez como inicio le pasamos el inicio del Array y como 
+     * final la mitad de nuestro Array - 1. 
+     *   
+     * @param Los parametros que le pasamos son: Un Array de numeros enteros, una posicion inicial,
+     * 		  una posicion final y el valor a buscar.
+     * 
+     * @param int [] lista
+     * @param int inicio
+     * @param int fin
+     * @param int x
+     * @return
+     */
     
     public static boolean busquedaBinaria(int[] lista, int inicio, int fin, int x) {
         int mitad;
@@ -111,6 +180,13 @@ public class Alg {
         }
     }
 
+    /**
+     * Este metodo nos permite realizar una comprobacion sobre los algoritmos a utilizar de forma
+     * que nos cercioremos de que funcionan correctamente para poder hacer pruebas con ellos.
+     * Para ello realizamos tests con Arrays predeterminados de forma que podamos verificarlo 
+     * correctamente.
+     * 
+     */
     public static void algTests (){
     	int[] listaMerge = {5,7,8,1,4,9,6,2};
     	int[] listaQuick = {5,7,8,1,4,9,6,2};
@@ -150,6 +226,18 @@ public class Alg {
     	System.out.println("Busco el 3 (no esta en la lista) " + busquedaBinaria(listaBin,0,listaBin.length-1,3));
     }
     
+    /**
+     * El siguiente metodo crea, a traves de bucles, 100 Arrays de numeros enteros los cuales
+     * posteriormente seran reyenados con un numero T de elementos el cual le pasamos por parametro
+     * al metodo.
+     * A continuacion lo que nos permite es para cada uno de los Arrays calcular el tiempo de ejecución 
+     * que tarda en aplicar el algoritmo de ordenacion MergeSort y la memoria utilizada por este.
+     * Seguidamente se crea un archivo Excel.csv en el cual se almancenan dichos datos de forma que podamos
+     * trabajar con ellos
+     *  
+     * @param El parametro que utilizamos es un entero que es el numero de elementos de cada Array.
+     * @param int t
+     */
     public static void tiempoMerge (int t){
         int[] array;
         
@@ -199,6 +287,19 @@ public class Alg {
         System.out.println("MergeSort done!");
     }
     
+    
+    /**
+     * El siguiente metodo crea, a traves de bucles, 100 Arrays de numeros enteros los cuales
+     * posteriormente seran reyenados con un numero T de elementos el cual le pasamos por parametro
+     * al metodo.
+     * A continuacion lo que nos permite es para cada uno de los Arrays calcular el tiempo de ejecución 
+     * que tarda en aplicar el algoritmo de ordenacion QuickSort y la memoria utilizada por este.
+     * Seguidamente se crea un archivo Excel.csv en el cual se almancenan dichos datos de forma que podamos
+     * trabajar con ellos
+     *  
+     * @param El parametro que utilizamos es un entero que es el numero de elementos de cada Array.
+     * @param int t
+     */
     public static void tiempoQuick (int t){
         int[] array;
         
@@ -242,6 +343,18 @@ public class Alg {
         System.out.println("done!");
     }
     
+    /**
+     * El siguiente metodo crea, a traves de bucles, 100 Arrays de numeros enteros los cuales
+     * posteriormente seran reyenados con un numero T de elementos el cual le pasamos por parametro
+     * al metodo.
+     * A continuacion lo que nos permite es para cada uno de los Arrays calcular el tiempo de ejecución 
+     * que tarda en aplicar el algoritmo de busqueda BinarySearch .
+     * Seguidamente se crea un archivo Excel.csv en el cual se almancenan dichos datos de forma que podamos
+     * trabajar con ellos
+     *  
+     * @param El parametro que utilizamos es un entero que es el numero de elementos de cada Array.
+     * @param int t
+     */
     public static void tiempoBinary(int t){
         int[] array;
         
@@ -283,6 +396,18 @@ public class Alg {
         System.out.println("done!");
     }
     
+    /**
+     * El siguiente metodo crea, a traves de bucles, 100 Arrays de numeros enteros los cuales
+     * posteriormente seran reyenados con un numero T de elementos el cual le pasamos por parametro
+     * al metodo.
+     * A continuacion lo que nos permite es para cada uno de los Arrays calcular el tiempo de ejecución 
+     * que tarda en aplicar el algoritmo de busqueda SecuentialSearch.
+     * Seguidamente se crea un archivo Excel.csv en el cual se almancenan dichos datos de forma que podamos
+     * trabajar con ellos
+     *  
+     * @param El parametro que utilizamos es un entero que es el numero de elementos de cada Array.
+     * @param int t
+     */
     public static void tiempoSecuencial(int t){
     int[] array;
         
@@ -324,12 +449,18 @@ public class Alg {
     }
     
 
-    
+    /**
+     * El main de esta clase lo que nos permite es, a traves de un entero tamano, imprimir por pantalla
+     * los valores de tiempo y memoria que utilizan nuestros distintos metodos.
+     * El entero tamano sera modificado entre valores de 5000, 7500, 10000 y 12500 para obtener distintos
+     * resultados y poder hacer pruebas con ellos.
+     * 
+     */
     public static void main(String[] args) {
        
     	//algTests();
     	
-    	int tamano = 12500;
+    	int tamano = 5000;
     	
     	tiempoQuick(tamano);
     	System.out.println("___________________________________________________________________________");
